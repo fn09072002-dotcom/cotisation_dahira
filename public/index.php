94ea51af65f3327465d601dd0e3edea0c276d5f4
fatou <fn09072002@gmail.com>
2026-09-11 15:55:11 +0000

    Allow Railway environment variables without env file

--- public/index.php
+++ public/index.php
@@ -5,7 +5,7 @@ require __DIR__ . '/../vendor/autoload.php';
 use Dotenv\Dotenv;
 
 $dotenv = Dotenv::createImmutable(__DIR__ . '/..');
-$dotenv->load();
+$dotenv->safeLoad();
 
 use App\Core\Database;
 
