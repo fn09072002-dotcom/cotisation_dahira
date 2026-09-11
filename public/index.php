dda4d2ec65c13dd752667aac867dc66665b923f4
fatou <fn09072002@gmail.com>
2026-09-11 18:39:08 +0000

    Redirect homepage to login

--- public/index.php
+++ public/index.php
@@ -1,18 +1,4 @@
 <?php
 
-require __DIR__ . '/../vendor/autoload.php';
-
-use Dotenv\Dotenv;
-
-$dotenv = Dotenv::createImmutable(__DIR__ . '/..');
-$dotenv->safeLoad();
-
-use App\Core\Database;
-
-// Test rapide de connexion
-try {
-    $db = Database::getConnection();
-    echo "Connexion à la base réussie !";
-} catch (Exception $e) {
-    echo "Erreur : " . $e->getMessage();
-}
+header('Location: /login.php');
+exit;
